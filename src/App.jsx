b96c4a43d37129d42c7f931c29e865372df2d7e7
Stephanie <cherrygurl5000@yahoo.com>
2026-01-08 12:59:15 -0500

    All styling and conditional styling has been completed for the movie cards.

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,71 @@ import Search from './components/Search';
 import Spinner from './components/Spinner';
 
 function App() {
-    const posterLink =
-        'https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-    const movie = {
-        title: 'Jumanji',
-        poster_path: posterLink,
-        vote_average: 4.5,
-        release_date: '1/6/2026',
-        original_language: 'English'
-    };
+    const posterLink = [
+        'https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdmllfGVufDB8fDB8fHww',
+        'https://images.unsplash.com/photo-1626814026160-2237a95fc5a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW92aWV8ZW58MHx8MHx8fDA%3D'
+    ];
+    const movies = [
+        {
+            title: 'Jumanji',
+            poster_path: posterLink[0],
+            vote_average: 4.5,
+            release_date: '1/6/2026',
+            original_language: 'English'
+        },
+        {
+            title: 'Mars Needs Moms',
+            poster_path: posterLink[1],
+            vote_average: 4.3,
+            release_date: '1/7/2025',
+            original_language: 'Spanish'
+        },
+        {
+            title: 'Lion King',
+            poster_path: posterLink[2],
+            vote_average: 4.8,
+            release_date: '2/16/2024',
+            original_language: 'French'
+        },
+        {
+            title: 'Lion King : The Musical Number',
+            poster_path: posterLink[2],
+            vote_average: 4.8,
+            release_date: '2/16/2024',
+            original_language: 'French'
+        }
+    ];
     return (
         <>
-            <MovieCard movie={movie} />
+            <h2>Trending List :</h2>
+            <div className="row d-flex justify-content-center">
+                {movies.map((movie, index) => (
+                    <MovieCard movie={movie} key={index} />
+                ))}
+            </div>
+
+            <h2>Popular: </h2>
+            <div className="row">
+                {movies.map((movie, index) => (
+                    <MovieCard movie={movie} key={index} />
+                ))}
+            </div>
+
+            <div className="row">
+                <div className="col-2 text-truncate">
+                    This text is quite long, and will be truncated once
+                    displayed.
+                </div>
+            </div>
+
+            <span
+                className="d-inline-block text-truncate"
+                style={{ maxWidth: '150px' }}
+            >
+                This text is quite long, and will be truncated once displayed.
+            </span>
+
             <Search />
             <Spinner />
         </>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,30 +1,68 @@
 import React from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export default function MovieCard({
     movie: { title, vote_average, poster_path, release_date, original_language }
 }) {
+    // Movie title text width
+    const ref = useRef(null);
+    const [width, setWidth] = useState(0);
+    const [textWidth, setTextWidth] = useState(0);
+
+    // Language text width
+    const ref2 = useRef(null);
+    const [langWidth, setLangWidth] = useState(0);
+    const [langTextWidth, setLangTextWidth] = useState(0);
+
+    useLayoutEffect(() => {
+        setWidth(ref.current.offsetWidth);
+        setTextWidth(ref.current.scrollWidth);
+        setLangWidth(ref.current.offsetWidth);
+        setLangTextWidth(ref.current.scrollWidth);
+    }, []);
+
     return (
-        <div className="d-flex flex-row justify-content-center">
-            <div className="card " style={{ width: '18rem' }}>
+        <div className="text-center col col-lg-3 my-4">
+            <div className="card " style={{ width: '18rem', height: '100%' }}>
                 <img
                     src={poster_path}
                     className="card-img-top border-bottom"
                     alt="{title} Poster"
                     style={{
                         width: '18rem',
+                        height: '18rem',
                         position: 'relative',
                         left: '-2rem',
                         top: '-2rem'
                     }}
                 />
-
                 <div className="card-body" style={{ paddingTop: '0' }}>
-                    <h2 className="card-title text-center">{title}</h2>
-                    <p className="card-text text-center">
+                    <h2
+                        className={`card-title text-center pb-2 ${
+                            textWidth > width
+                                ? textWidth > width * 2 - 10
+                                    ? 'fs-6 text-wrap mb-2 b-1'
+                                    : 'fs-4 overflow-visible pt-2 mb-3'
+                                : 'fs-1'
+                        }`}
+                        ref={ref}
+                    >
+                        {title}
+                    </h2>
+                    <div className="card-text text-center">
                         <p>{vote_average} Stars</p>
                         <p>Released: {release_date}</p>
-                        <p>Original Language: {original_language}</p>
-                    </p>
+                        <p
+                            className={
+                                langTextWidth > langWidth
+                                    ? 'pt-1 pb-2 mb-4 small'
+                                    : ''
+                            }
+                            ref={ref2}
+                        >
+                            Original Language: {original_language}
+                        </p>
+                    </div>
                     <a href="#" className="btn btn-primary mt-2">
                         View More
                     </a>
